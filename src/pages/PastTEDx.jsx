7d3deda4982ae99a3311374368c7f274733c1d73
sarthakbaisyasahaaiimsk-jpg <sarthakbaisyasahaaiimsk@gmail.com>
2026-08-21 23:40:49 +0530

    Fix speaker image reference for Dr. Sumer Sethi

--- src/pages/PastTEDx.jsx
+++ src/pages/PastTEDx.jsx
@@ -27,7 +27,7 @@ const pastEvent = {
   speakers: [
     { name: "Col. Prof. (Dr.) Ajay Mallick", title: "Paradox In Paradise", talkUrl: "https://youtu.be/o3Q4uG_W8Do?si=uIegVmTnRca2D5SB", image: ajay },
     { name: "Dr. Tara Rajendran", title: "Grow Without A map: Start From Nothing, Build What Matters", talkUrl: "https://youtu.be/vIbydZZGCJo?si=Dgeci1euvToeYsmK", image: tara },
-    { name: "Dr. Sumer Sethi", title: "The Blackboard; Hidden Journey of a Teacher", talkUrl: "https://youtu.be/2oSD69mtdME?si=CCer6VNMgP1TofvW", image: sumera },
+    { name: "Dr. Sumer Sethi", title: "The Blackboard; Hidden Journey of a Teacher", talkUrl: "https://youtu.be/2oSD69mtdME?si=CCer6VNMgP1TofvW", image: sumer },
     { name: "Dr. Ankita Pandey", title: "Beyond the Cage", talkUrl: "https://youtu.be/17Vo4pbLsEI?si=Eg4kjnjpDOlTvexJ", image: ankita },
     { name: "Dr. Sasi Kanta Dash", title: "Barren to Bloom: The Power of a Sapling", talkUrl: "https://youtu.be/xjr2Ri-Yh90?si=bw3kM3lLyizc4EX4", image: sasi },
     { name: "Dr. Priyanka Chatterjee", title: "Burnt Blueprint", talkUrl: "https://youtu.be/_w-zMP95oJY?si=FEj7lZxrNmvDq9Qh", image: priyanka },
