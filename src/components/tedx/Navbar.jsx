16de4347a73ac3af077659c3a1e250fe0f434367
sarthakbaisyasahaaiimsk-jpg <sarthakbaisyasahaaiimsk@gmail.com>
2026-08-23 16:57:23 +0530

    Update Navbar.jsx

--- src/components/tedx/Navbar.jsx
+++ src/components/tedx/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
           scrolled ? "bg-black/90 backdrop-blur-md border-b border-white/5" : "bg-transparent"
         }`}
       >
-        <div className="max-w-7xl mx-auto px-6 lg:px-12 flex items-center justify-between h-16 lg:h-20">
+        <div className="max-w-7xl mx-auto px-6 lg:px-12 flex items-center h-16 lg:h-20">
 
           <Link to="/" className="flex items-center">
             <img src={logo} alt="TEDxAIIMS Kalyani" className="h-16 w-auto" />
